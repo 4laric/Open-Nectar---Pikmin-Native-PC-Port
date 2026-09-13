0b31906d7d4db1db86f73e1c2b776a87aae40167
Alaric McKenzie-Boone <alaric.mckenzie.boone@gmail.com>
2026-09-13 16:43:56 -0400

    Add shared texture SRT sampler and scoped material draw adapter (#391)

---
+++ tools/material_srt_stubs/Material.h
@@ -0,0 +1,11 @@
+#pragma once
+struct Matrix4f { float mMtx[4][4]{}; };
+struct PVWTextureData {
+ unsigned _UNUSED10=0,mAnimationFactor=255,mTotalFrameCount=0;
+ float mScaleX=1,mScaleY=1,mRotationZ=0,mTranslationX=0,mTranslationY=0,mPivotX=0,mPivotY=0;
+ Matrix4f mAnimatedTexMtx;
+};
+struct PVWTexGenData { unsigned mTexCoordID=0,mTexGenType=1,mTexGenSrc=4,mMatrixType=10; };
+struct PVWTextureInfo { int mTextureDataCount=1,mTexGenDataCount=1;PVWTextureData* mTextureData=nullptr;PVWTexGenData* mTexGenData=nullptr; };
+struct Material { PVWTextureInfo mTextureInfo; };
+class Graphics;
