a7564f535536afc2733b21bf970d64728d07ffe1
Alaric McKenzie-Boone <alaric.mckenzie.boone@gmail.com>
2026-09-13 17:01:45 -0400

    Clear Demon bindings with Teki manager lifecycle

--- src/plugPikiNakata/tekimgr.cpp
+++ src/plugPikiNakata/tekimgr.cpp
@@ -20,6 +20,7 @@
 #include "pc_p2_armor.h"
 #include "pc_p2_batch3.h"
 #include "pc_p2_long_legs.h"
+#include "pc_p2_demon_host.h"
 #endif
 #include "DebugLog.h"
 #include "Dolphin/os.h"
@@ -131,6 +132,7 @@ int TekiMgr::typeIds[TEKI_TypeCount] = {
 void TekiMgr::initTekiMgr()
 {
 #if defined(PIKI_PC_PORT) && PIKI_PC_PORT
+	pc_p2_demon_manager_reset();
 	{ pc_p2_snow_reset(); pc_p2_sheargrub_reset(); pc_p2_kochappy_reset(); pc_p2_giant_breadbug_actor_reset(); pc_p2_breadbug_actor_reset(); pc_p2_queen_reset(); pc_p2_king_reset(); pc_p2_frog_reset(); pc_p2_kogane_reset(); pc_p2_mamuta_reset(); pc_p2_tank_reset(); pc_p2_qurione_reset(); pc_p2_kurage_teki_reset(); pc_p2_onikurage_teki_reset(); pc_p2_batch2_reset(); pc_p2_projectiles_reset(); pc_p2_sokkuri_reset(); pc_p2_armor_reset(); pc_p2_batch3_reset(); pc_p2_long_legs_reset(); }
 #endif
 	tekiMgr = nullptr;
@@ -157,6 +159,7 @@ int TekiMgr::getTypeIndex(immut char* typeName)
 TekiMgr::TekiMgr()
 {
 #if defined(PIKI_PC_PORT) && PIKI_PC_PORT
+	if (!tekiMgr) pc_p2_demon_manager_reset();
 	// Stage teardown nulls the global manager. Do not clear an unrelated live manager.
 	if (!tekiMgr) { pc_p2_snow_reset(); pc_p2_sheargrub_reset(); pc_p2_kochappy_reset(); pc_p2_giant_breadbug_actor_reset(); pc_p2_breadbug_actor_reset(); pc_p2_queen_reset(); pc_p2_king_reset(); pc_p2_frog_reset(); pc_p2_kogane_reset(); pc_p2_mamuta_reset(); pc_p2_tank_reset(); pc_p2_qurione_reset(); pc_p2_kurage_teki_reset(); pc_p2_onikurage_teki_reset(); pc_p2_batch2_reset(); pc_p2_projectiles_reset(); pc_p2_sokkuri_reset(); pc_p2_armor_reset(); pc_p2_batch3_reset(); pc_p2_long_legs_reset(); }
 #endif
@@ -289,6 +292,7 @@ Teki* TekiMgr::newTeki(int type)
 	}
 
 #if defined(PIKI_PC_PORT) && PIKI_PC_PORT
+	pc_p2_demon_manager_forget((BTeki*)teki);
 	pc_p2_snow_forget(teki); pc_p2_sheargrub_forget(teki); pc_p2_kochappy_forget(teki); pc_p2_giant_breadbug_actor_forget(teki); pc_p2_breadbug_actor_forget(teki); pc_p2_frog_forget(teki); pc_p2_kogane_forget(teki); pc_p2_mamuta_forget(teki); pc_p2_tank_forget(teki); pc_p2_qurione_forget(teki); pc_p2_kurage_teki_forget(teki); pc_p2_onikurage_teki_forget(teki); pc_p2_batch2_forget(teki); pc_p2_projectiles_forget(teki); pc_p2_sokkuri_forget(teki); pc_p2_armor_forget(teki); pc_p2_batch3_forget(teki); pc_p2_long_legs_forget(teki);
 #endif
 	teki->init(type);
@@ -304,6 +308,7 @@ Teki* TekiMgr::newTeki(int type)
 void TekiMgr::reset()
 {
 #if defined(PIKI_PC_PORT) && PIKI_PC_PORT
+	if (this == tekiMgr) pc_p2_demon_manager_reset();
 	if (this == tekiMgr) { pc_p2_snow_reset(); pc_p2_sheargrub_reset(); pc_p2_kochappy_reset(); pc_p2_breadbug_visual_reset(); pc_p2_giant_breadbug_visual_reset(); pc_p2_bulblax_visual_reset(); pc_p2_giant_breadbug_actor_reset(); pc_p2_breadbug_actor_reset(); pc_p2_queen_reset(); pc_p2_king_reset(); pc_p2_frog_reset(); pc_p2_kogane_reset(); pc_p2_mamuta_reset(); pc_p2_tank_reset(); pc_p2_qurione_reset(); pc_p2_kurage_teki_reset(); pc_p2_onikurage_teki_reset(); pc_p2_batch2_reset(); pc_p2_projectiles_reset(); pc_p2_sokkuri_reset(); pc_p2_armor_reset(); pc_p2_batch3_reset(); pc_p2_long_legs_reset(); }
 #endif
 	PRINT_NAKATA("reset>\n");
