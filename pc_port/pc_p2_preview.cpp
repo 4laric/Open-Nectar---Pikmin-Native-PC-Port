f8f186cf6692958d6d446efa7b41684b6d6e3857
Alaric McKenzie-Boone <alaric.mckenzie.boone@gmail.com>
2026-09-14 19:50:34 -0400

    lane29: [hook] credit Kurage corpse in the Pod delivery receipt path (#243)

--- pc_port/pc_p2_preview.cpp
+++ pc_port/pc_p2_preview.cpp
@@ -330,6 +330,9 @@ bool pc_p2_preview_deliver(Pellet* pellet) {
         else if(unsigned generator=0;pc_p2_mamuta_receipt(pellet->mPelletView,generator)) {
             receipt="corpse:"+pc_p2_cave_receipt_prefix()+"mamuta:"+std::to_string(generator);value=corpseValue;
         }
+        else if(unsigned generator=0;pc_p2_kurage_receipt(pellet->mPelletView,generator)) {
+            receipt="corpse:"+pc_p2_cave_receipt_prefix()+"kurage:"+std::to_string(generator);value=corpseValue;
+        }
         else {
             auto found=corpses.find(pellet->mPelletView);
             if(found==corpses.end()) {std::fprintf(stderr,"Unregistered P2 pod cargo id=%08x view=%p pellet=%p treasure=%p; refusing seed side effects\n",pellet->mConfig->mModelId.mId,(void*)pellet->mPelletView,(void*)pellet,(void*)previewTreasure);std::abort();}
