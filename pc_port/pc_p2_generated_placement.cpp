761df6580e18ef307e3bbb4bca9288de8d2c72b8
Alaric McKenzie-Boone <alaric.mckenzie.boone@gmail.com>
2026-09-15 12:34:40 -0400

    lane22/04: generated-placement bridge covers elemental Otakara 59-62 (#170/#447)

--- pc_port/pc_p2_generated_placement.cpp
+++ pc_port/pc_p2_generated_placement.cpp
@@ -1,5 +1,6 @@
 #include "pc_p2_generated_placement.h"
 #include "pc_p2_sarai_manager.h"
+#include "pc_p2_otakara.h"
 #include "teki.h"
 #include <cstdio>
 
@@ -10,6 +11,16 @@ bool pc_p2_generated_placement_bind(BTeki* actor, unsigned sourceId, unsigned se
     case 23: // Swooping Snitchbug (Sarai); lane 30.
         if (pc_p2_sarai_manager_bind_dynamic(actor, generatorId, seedTargetUid)) {
             std::printf("P2_GENERATED_PLACEMENT source_id=23 target=%u bound=1\n", seedTargetUid);
+            std::fflush(stdout);
+            return true;
+        }
+        return false;
+    case 59: // elemental Otakara Dweevils; lane 22.
+    case 60:
+    case 61:
+    case 62:
+        if (pc_p2_otakara_bind_dynamic(actor, generatorId, sourceId)) {
+            std::printf("P2_GENERATED_PLACEMENT source_id=%u target=%u bound=1\n", sourceId, seedTargetUid);
             std::fflush(stdout);
             return true;
         }
