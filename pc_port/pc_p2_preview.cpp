014eb9c60195227bbaa9cdc1b05064de9c48c78f
Alaric McKenzie-Boone <alaric.mckenzie.boone@gmail.com>
2026-09-15 01:53:02 -0400

    lane24: hook King Teki param seam (teki.h) + Pod corpse:king: receipt (#445)

--- pc_port/pc_p2_preview.cpp
+++ pc_port/pc_p2_preview.cpp
@@ -9,6 +9,7 @@
 #include "pc_p2_shijimi.h"
 #include "pc_p2_kurage_teki.h"
 #include "pc_p2_sheargrub.h"
+#include "pc_p2_king_teki.h"
 #include "pc_p2_kochappy.h"
 #include "pc_p2_dwarf_orange.h"
 #include "pc_p2_kochappy_fsm.h"
@@ -329,6 +330,9 @@ bool pc_p2_preview_deliver(Pellet* pellet) {
         }
         else if(unsigned generator=0;pc_p2_mamuta_receipt(pellet->mPelletView,generator)) {
             receipt="corpse:"+pc_p2_cave_receipt_prefix()+"mamuta:"+std::to_string(generator);value=corpseValue;
+        }
+        else if(unsigned generator=0;pc_p2_king_teki_receipt(pellet->mPelletView,generator)) {
+            receipt="corpse:"+pc_p2_cave_receipt_prefix()+"king:"+std::to_string(generator);value=corpseValue;
         }
         else {
             auto found=corpses.find(pellet->mPelletView);
