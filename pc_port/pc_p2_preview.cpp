400d7b45c2cc3f1b7d169a131149205acb131357
Alaric McKenzie-Boone <alaric.mckenzie.boone@gmail.com>
2026-09-17 04:14:45 -0400

    species-integration: fix stray brace that broke pc_p2_preview deliver chain (introduced in gen48 mar-arm merge; reported via #150)
    (cherry picked from commit 20abe41b14ab99995d40b63228b75a87d0682edc)

--- pc_port/pc_p2_preview.cpp
+++ pc_port/pc_p2_preview.cpp
@@ -396,7 +396,6 @@ bool pc_p2_preview_deliver(Pellet* pellet) {
         else if(unsigned generator=0;pc_p2_long_legs_receipt(pellet,generator)) {
             receipt="corpse:"+pc_p2_cave_receipt_prefix()+"longlegs:"+std::to_string(generator);value=corpseValue;
         }
-        }
         else {
             auto found=corpses.find(pellet->mPelletView);
             if(found==corpses.end()) {std::fprintf(stderr,"Unregistered P2 pod cargo id=%08x view=%p pellet=%p treasure=%p; refusing seed side effects\n",pellet->mConfig->mModelId.mId,(void*)pellet->mPelletView,(void*)pellet,(void*)previewTreasure);std::abort();}
